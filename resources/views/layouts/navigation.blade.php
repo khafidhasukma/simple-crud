6ff97616e9e6fdf544b796a990d6f8052f297b9b
khafidhasukma@gmail.com <khafidhasukma@gmail.com>
2023-06-08 04:20:35 +0700

    [add]: crud article

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('article.index')" :active="request()->routeIs('article.index')">
+                        {{ __('Article') }}
+                    </x-nav-link>
                 </div>
             </div>
 
